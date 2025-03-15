0c6ed5e3077b30aa820eeb8d97191274e627ac89
kimaniui <kimanifrank209@gmail.com>
2025-03-16 01:38:04 +0300

    testing

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -25,7 +25,8 @@ const Contact = () => {
   
     const serviceId = import.meta.env.VITE_EMAILJS_SERVICE_ID;
     const publicKey = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
-    console.log(serviceId, publicKey);
+    console.log("ServiceId", serviceId);
+    console.log("PublicKey", publicKey);
     const formData = new FormData(form.current);
     const formValues = Object.fromEntries(formData.entries());
   
@@ -34,8 +35,9 @@ const Contact = () => {
     // EmailJS Template IDs
     const userTemplateId = import.meta.env.VITE_EMAILJS_USER_TEMPLATE_ID;
     const adminTemplateId = import.meta.env.VITE_EMAILJS_ADMIN_TEMPLATE_ID;
-    console.log(userTemplateId, adminTemplateId);
-    
+    console.log("User Templateid", userTemplateId);
+    console.log("Admin Templateid", adminTemplateId);
+
     Promise.all([
       // Send to User
       emailjs.send(serviceId, userTemplateId, { 
